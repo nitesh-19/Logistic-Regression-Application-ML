df1d80f6d4ab236541f1476da281fd919fa6b2a8
nitesh-19 <niteshgohil25@gmail.com>
2023-09-09 22:37:44 +0530

    Added the cost function and gradient descent

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 def apply_logistic_regression(W, X, B):
     """
     Runs logistic regression on given parameters.
-
     :param W: Weights as a numpy array.
     :param X: Features as a numpy array.
     :param B: The offset term supplied as a float or an integer.
@@ -24,12 +23,22 @@ class LogisticRegression:
         :param target_index:
         :param values_to_replace:
         """
+        self.cost = None
+        self.alpha = 0.01
+        self.feature_column_names_list = None
         self.working_dataframe = None
         self.DATA_PATH = data_path
         self.features_index_list = feature_indexes
         self.target_index_list = target_index
         self.values_to_replace = values_to_replace
+        self.m = None
+        self.W = np.zeros(len(self.features_index_list))
+        self.B = 0
+
+        self.target_column_name = None
         self.build_training_dataframe()
+        print(self.get_feature_array(0))
+        self.run_trainer()
 
     def build_training_dataframe(self):
         """
@@ -51,15 +60,16 @@ class LogisticRegression:
                 f"training.")
         else:
             data = pd.read_csv(self.DATA_PATH)
-            data.dropna(axis=0, inplace=True)
             if self.values_to_replace is not None:
-                data = self.replace_values_with_numbers(data=data)
-            feature_column_names_list = [data.columns[index] for index in self.features_index_list]
-            target_column_name = data.columns[self.target_index_list]
-            self.working_dataframe = pd.DataFrame(data=data[feature_column_names_list]).copy()
-            self.working_dataframe[target_column_name] = data[target_column_name]
+                data = self.replace_strings_with_numbers(data=data)
+            self.feature_column_names_list = [data.columns[index] for index in self.features_index_list]
+            self.target_column_name = data.columns[self.target_index_list]
+            self.working_dataframe = pd.DataFrame(data=data[self.feature_column_names_list]).copy()
+            self.working_dataframe[self.target_column_name] = data[self.target_column_name]
+            self.working_dataframe.dropna(axis=0, inplace=True)
+            self.m = self.working_dataframe.shape[0]
 
-    def replace_values_with_numbers(self, data):
+    def replace_strings_with_numbers(self, data):
         """
         Replaces the string values in dataframe with corresponding numerical values as supplied in the
         "values_to_replace" argument.
@@ -70,3 +80,55 @@ class LogisticRegression:
             for inner_key in self.values_to_replace[key]:
                 data[key].replace(inner_key, self.values_to_replace[key][inner_key], inplace=True)
         return data
+
+    def cost_function(self):
+        summation = 0
+        for integer in range(0, self.m):
+            summation += self.working_dataframe.iloc[integer][self.target_column_name] * np.log10(
+                apply_logistic_regression(self.W, self.get_feature_array(integer), self.B)) + (
+                                 1 - self.working_dataframe.iloc[integer][self.target_column_name]) * np.log10(
+                1 - apply_logistic_regression(self.W, self.get_feature_array(integer), self.B))
+        self.cost = summation * (-1 / self.m)
+        return self.cost
+
+    def gradient_descent(self):
+        # temp_W = self.W - self.alpha*()
+        sum_of_W = 0
+        sum_of_B = 0
+        temp_W = np.zeros(len(self.features_index_list))
+
+        for j in range(0, len(self.feature_column_names_list)):
+            for i in range(0, self.m):
+                X_array = self.get_feature_array(i)
+                sum_of_W += apply_logistic_regression(self.W, X_array, self.B) - \
+                            self.working_dataframe.iloc[i][self.target_column_name]* X_array[j]
+            sum_of_W /= self.m
+            reduction_term_W = sum_of_W * self.alpha
+            temp_W[j] = self.W[j] - reduction_term_W
+
+        for k in range(0, self.m):
+            X_array = self.get_feature_array(k)
+            sum_of_B += apply_logistic_regression(self.W, X_array, self.B) - \
+                        self.working_dataframe.iloc[k][self.target_column_name]
+            sum_of_B /= self.m
+            reduction_term_B = sum_of_B * self.alpha
+            temp_B = self.B - reduction_term_B
+
+        self.W = temp_W
+        self.B = temp_B
+        self.cost_function()
+
+    def get_feature_array(self, index):
+        return np.array(self.working_dataframe.iloc[index][self.feature_column_names_list])
+
+    def run_trainer(self):
+        flag = True
+        prev_cost = 0
+        while flag:
+            self.gradient_descent()
+            print(self.W, self.B, self.cost)
+
+            if self.cost == prev_cost:
+                print(self.W, self.B, self.cost)
+                flag = False
+            prev_cost = self.cost
