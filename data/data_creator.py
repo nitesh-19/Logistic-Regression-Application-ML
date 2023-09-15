9b54738bcb3e2f060c7ac4d4c8a19e254f395b92
nitesh-19 <niteshgohil25@gmail.com>
2023-09-15 12:52:23 +0530

    Code to create test data according to specs

---
+++ data/data_creator.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+
+data = pd.DataFrame(columns=["hours", "iq", "result"], index=range(0, 100))
+
+for i in range(100):
+    hours_studied = round(random.random() * 8,2)
+    iq = round(random.random() * 100)
+    if hours_studied <= 3:
+        result = 0
+    elif iq <= 30:
+        result = 0
+    else:
+        result = 1
+    data.iloc[i]["hours"] = hours_studied
+    data.iloc[i]["iq"] = iq
+    data.iloc[i]["result"] = result
+
+print(data)
+plt.scatter(data["hours"], data["iq"], c=data["result"])
+plt.show()
+data.to_csv("exam_data.csv")
