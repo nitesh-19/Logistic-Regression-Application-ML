Fixed bugs in gradient descent

logistic_regression.py
@@ -12,12 +12,24 @@ def apply_logistic_regression(W, X, B):
     :param B: The offset term supplied as a float or an integer.
     :return: The output of the Logistic Regressio Function between 1 and 0.
     """
+
     return 1 / (1 + np.exp(-((np.dot(W, X)) + B)))
 
 
-def plot_result(dataframe, X, Y):
+def implement_logistic_regression_model(W, X, B, i):
+    return -((W[0] * X[i] + B) / W[1])
+
+
+def plot_result(dataframe, X, Y, W, B):
+    list_of_y = []
+    list_of_x = []
+    for i in range(0, int(dataframe["hours"].max()) + 1):
+        list_of_x.append(i)
+        list_of_y.append(implement_logistic_regression_model(W, X, B, i))
+
     fig1 = plt.figure("figure 1")
     plt.title("data")
+    plt.plot(list_of_x, list_of_y, "o")
     plt.scatter(dataframe["hours"], dataframe["iq"], c=dataframe["result"], marker="x")
     fig2 = plt.figure("figure 2")
     plt.title("Cost")
@@ -36,7 +48,7 @@ class LogisticRegression:
         :param values_to_replace:
         """
         self.cost = None
-        self.alpha = 0.003
+        self.alpha = 0.1
         self.scale_factors = []
         self.feature_column_names_list = None
         self.working_dataframe = None
@@ -45,10 +57,10 @@ class LogisticRegression:
         self.target_index_list = target_index
         self.values_to_replace = values_to_replace
         self.m = None
-        # self.W = np.zeros(len(self.features_index_list))
-        self.W = np.array([0.20119254, -0.00244896])
+        self.W = np.zeros(len(self.features_index_list))
+        self.W = np.array([0.84721055, 1.05935965])
         # self.B = 0
-        self.B = -0.3066199069627153
+        self.B = -6.713996364075926
         self.target_column_name = None
         self.build_training_dataframe()
         self.run_trainer()
@@ -141,19 +153,23 @@ class LogisticRegression:
         Runs Gradient descent on the model and updates the weights.
         :return:
         """
-        sum_of_W = 0
         sum_of_B = 0
+        sum_of_W = np.zeros(len(self.features_index_list))
+        for j in range(len(self.features_index_list)):
+            for i in range(self.m):
+                X_array = self.get_feature_array(i)
+                sum_of_W[j] += (apply_logistic_regression(self.W, X_array, self.B) -
+                                self.working_dataframe.iloc[i][self.target_column_name]) * X_array[j]
+        sum_of_W /= self.m
+        sum_of_W *= self.alpha
 
-        for i in range(0, self.m):
-            X_array = self.get_feature_array(i)
-            sum_of_W += (apply_logistic_regression(self.W, X_array, self.B) -
-                         self.working_dataframe.iloc[i][self.target_column_name]) * X_array
-            sum_of_B += apply_logistic_regression(self.W, X_array, self.B) - self.working_dataframe.iloc[i][
+        for k in range(0, self.m):
+            X_array = self.get_feature_array(k)
+
+            sum_of_B += apply_logistic_regression(self.W, X_array, self.B) - self.working_dataframe.iloc[k][
                 self.target_column_name]
 
-        sum_of_W /= self.m
-        reduction_term_W = sum_of_W * self.alpha
-        self.W = self.W - reduction_term_W
+        self.W = self.W - sum_of_W
         sum_of_B /= self.m
         reduction_term_B = sum_of_B * self.alpha
         self.B = self.B - reduction_term_B
@@ -179,7 +195,7 @@ class LogisticRegression:
                 self.gradient_descent()
                 print(f"W: {self.W}, B: {self.B}, Cost: {self.cost}")
                 iterations_finished += 1
-                if iterations_finished % 2 == 0:
+                if iterations_finished % 1 == 0:
                     cost_history.append(self.cost)
                     iterations_history.append(iterations_finished)
 
@@ -189,4 +205,6 @@ class LogisticRegression:
                 prev_cost = self.cost
 
         except KeyboardInterrupt:
-            plot_result(self.working_dataframe, iterations_history, cost_history)
+            plot_result(self.working_dataframe, iterations_history, cost_history, self.W, self.B)
+        else:
+            plot_result(self.working_dataframe, iterations_history, cost_history, self.W, self.B)